ebf61f774c9cbb5f9ecc545c2bdc42fff2678ff4
SunM1sty <mis7akes@mail.ru>
2023-10-28 14:59:59 +0300

    feat: transfer from webpack build tool to tsup

--- config/build-plugins.ts
+++
@@ -1,8 +0,0 @@
-import webpack from 'webpack';
-import {BuildOptions} from "./types/config";
-
-export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-        new webpack.ProgressPlugin()
-    ]
-}
